3b83ca1b1bc4991e55b7e560780673fbf6c57825
Murial Anindo <twentymurial@outlook.com>
2018-04-28 07:31:47 -0400

    second commit

--- liri.js
+++ liri.js
@@ -56,14 +56,15 @@ var getMySpotify = function(songName) {
       }
     });
 }
-
 var getMyMovies=function(){
-  request('http://www.omdbapi.com/?t=pulp+fiction', function (error, response, body) {
-    if(!error && response.statusCode == 200) {
-      console.log(body);
-}
+  request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+  }
+
   })
-}
+};
+
 var runCommand=function(){
   fs.readFile('random.txt', 'UTF8',function(err, data) {
     if (err) throw err;
